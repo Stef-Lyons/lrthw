3a153621117008fd2b769684a59afee64aedf3f4
Stef-Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-01 20:51:39 +0000

    Add files via upload

---
+++ ex6.rb
@@ -0,0 +1,37 @@
+# The line below means the variable called types_of_people has the value of 10.
+types_of_people = 10
+# The line below means the variable of x has the value of a string which will
+# contain the value of the variable types_of_people interpolated in it when it
+#is printed.
+x = "There are #{types_of_people} types of people."
+# The line below means the variable named binary has the strng value of "binary".
+binary = "binary"
+# The line below means the variable do_not has the value of the string "don't".
+do_not = "don't"
+# The line below means that the variable y has the value of a string which will
+# have the variables binary and do_not interpolated inside.
+y = "Those who know #{binary} and those who #{do_not}."
+
+# The line below means that the value of x will be printed to the screen.
+puts x
+# The line below means that the value of y will be printed to the screen.
+puts y
+
+# The line below means that the variable hilarious has the value of false.
+hilarious = false
+# The line below means that the variable joke_evaluation has the value of the
+# string with the variable hilarious interpolated in.
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# The line below means that the string value of joke_evaluation will be printed.
+puts joke_evaluation
+
+# The line below means that the variable w has the value of the corresponding
+# string.
+w = "This is the left side of..."
+# The line below means that the variable e has the value of the corresponding
+# string.
+e = "a string with a right side."
+
+# The line below means that the sum of variables w and e will be printed.
+puts w + e
