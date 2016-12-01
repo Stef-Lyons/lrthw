16f396c5a97db5d696746468b7a92e8078482fc9
Stef-Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-01 19:34:33 +0000

    Add files via upload

---
+++ ex3.rb
@@ -0,0 +1,45 @@
+# The line below will puts the string "I will now ocunt my chickens:"
+puts "I will now count my chickens:"
+
+# The line below will puts the string below with the word "Hens" and
+# then the results of the equation in the curly brackets.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# The line below will puts the string below with the word "Roosters" and
+# then the results of the equation in the curly brackets.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# This line will puts the string "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+
+#The line below will puts the results of the equation below (7)
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# The line below will puts the string exactly as written -
+# It will not solve the equation as it is part of the string.
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# The line below will puts the answer to the equation (false)
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# The line below will will puts the string "What is 3 + 2?" and after it
+# puts the answer to the equation in curly brackets, not the equation itself (5)
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# The line below will puts the string "What is 5 - 7?" and after it
+# will puts the answer to the equation in curly brackets, not the equation itself (-2)
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# The line below will puts the string "Oh, that's why it's false."
+puts "Oh, that's why it's false."
+
+# The line below will puts the string "How about some more?"
+puts "How about some more?"
+
+# The line below will puts the words in the string and then the answer
+# to the equation in curly brackets (true).
+puts "Is it greater? #{5.0 > -2.0}"
+# The line below will puts the words in the string and then the answer
+# to the equation in curly brackets (true).
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# The line below will puts the words in the string and then the answer
+# to the equation in curly brackets. (false)
+puts "Is it less or equal? #{5.0 <= -2.0}"
