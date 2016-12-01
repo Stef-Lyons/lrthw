3ac55fce4d76e1cec0cd26b2a2b85ad0d8490754
Stef-Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-01 21:09:27 +0000

    Add files via upload

---
+++ ex7.rb
@@ -0,0 +1,41 @@
+# The line below means that the following string will be printed.
+puts "Mary had a little lamb."
+# The line below means that the following string will be printed.
+puts "Its fleece was white as #{'snow'}."
+# The line below means that the following string will be printed.
+puts "And everywhere that Mary went."
+# The line below means that a string (.) will be done 10 times.
+puts "." * 10
+
+# The line below means that the variable end1 has the value of "C".
+end1 = "C"
+# The line below means that the variable end2 has the value of "h".
+end2 = "h"
+# The line below means that the variable end3 has the value of "e".
+end3 = "e"
+# The line below means that the variable end4 has the value of "e".
+end4 = "e"
+# The line below means that the variable end5 has the value of "s".
+end5 = "s"
+# The line below means that the variable end6 has the value of "e".
+end6 = "e"
+# The line below means that the variable end7 has the value of "B".
+end7 = "B"
+# The line below means that the variable end8 has the value of "u".
+end8 = "u"
+# The line below means that the variable end9 has the value of "r".
+end9 = "r"
+# The line below means that the variable end10 has the value of "g".
+end10 = "g"
+# The line below means that the variable end11 has the value of "e".
+end11 = "e"
+# The line below means that the variable end12 has the value of "r".
+end12 = "r"
+
+# The line below means that the sum of end1, end2, end3, end4, end5, and end6
+# printed to the screen without a line after (print not puts) so that the next
+# line prints straight after.
+print end1 + end2 + end3 + end4 + end5 + end6
+# The line below means that the sum of end7, end8, end9, end10, end11 and end12
+# is printed to the screen.
+puts end7 + end8 + end9 + end10 + end11 + end12
