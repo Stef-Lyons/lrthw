dc86d347b0ff23ac187f53a9467f2deba2dbe5c8
Stef-Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-02 09:54:36 +0000

    Add files via upload

---
+++ ex12extras.rb
@@ -0,0 +1,4 @@
+print "Give me a number:"
+number = gets.chomp.to_f
+
+puts "Thanks #{number} is a great number!"
---
+++ ex12extras2.rb
@@ -0,0 +1,8 @@
+print "Can I have some money please?"
+money = gets.chomp.to_f
+
+print "I will give you some change! "
+
+change = money * 0.1
+
+puts "You gave me #{money} and your change is #{change}. Thanks!"
