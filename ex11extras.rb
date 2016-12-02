949fe2bda631ab6edda2f28fd0b264052d6d6dda
Stef-Lyons <stefanie.hodges@yahoo.co.uk>
2016-12-02 09:41:10 +0000

    Add files via upload

---
+++ ex11extras.rb
@@ -0,0 +1,7 @@
+print "Where do you live?"
+location = gets.chomp
+print "How long have you lived there?"
+time = gets.chomp
+
+
+puts "You live in #{location}, you have lived there for #{time}. Great! "
